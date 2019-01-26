69d9b2adb86955db2f40aa2ff99657d8dacf1811
Abass <abs4real16.ma@gmail.com>
2019-01-26 19:52:09 +0100

    added some routes

--- blogapp/routes.py
+++ blogapp/routes.py
@@ -1,32 +1,18 @@
 import os
 import time
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from blogapp import app, db, bcrypt
-from blogapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from blogapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from blogapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-
-posts = [
-    {
-        'author': 'Abass Mak',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_created': 'April 20, 2018'
-    },
-    {
-        'author': 'Makinde Ade',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_created': 'April 21, 2018'
-    }
-]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_created.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -66,6 +52,12 @@ def login():
             next_page = request.args.get('next')
             flash('Logged in successful.', 'success')
             return redirect(next_page) if next_page else redirect(url_for('home'))
+        if user and not bcrypt.check_password_hash(user.password, form.password.data):
+            flash('Login Unsuccessful. Please check your password', 'danger')
+            return redirect(url_for('login'))
+        if not user:
+            flash('Failed!, it seems you are not a member. ', 'danger')
+            return redirect(url_for('register'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -108,3 +100,65 @@ def account():
         form.email.data == current_user.email
     image_file = url_for('static', filename='profile_pics/'  + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('The post was created successfully', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+
+@app.route("/post/<int:post_id>", methods=['GET'])
+@login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_created.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
